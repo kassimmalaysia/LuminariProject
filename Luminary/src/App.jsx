8d6cdbfa490c9a3b7f492077b527ad74d004ce03
kassimmalaysia <101180214+kassimmalaysia@users.noreply.github.com>
2023-03-18 13:35:44 +0800

    Revert "sihan"
    This reverts commit 46f8a424b6932a86c7a480015ad7d1a4df56a7a9.

--- Luminary/src/App.jsx
+++ Luminary/src/App.jsx
@@ -1,21 +1,34 @@
-import Index from "./pages/Index";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import TimetablePage from "./pages/TimetablePage";
-import SideBar from "./widgets/SideBar";
-import NullPage from "./components/NullPage";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
 
-function App(){
+function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<SideBar />} />
-          <Route path="/Timetable" element={<TimetablePage />} />
-          <Route path="/*" element={<NullPage />}/>
-        </Routes>
-      </BrowserRouter>
-    </>
-    
-  );
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
 }
+
 export default App
